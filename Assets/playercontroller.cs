112a67c648298d6ec95e3297fb7e8bf0a79e846d
ebtrinh <99101731+ebtrinh@users.noreply.github.com>
2025-07-20 17:45:25 -0400

    grug

--- Assets/playercontroller.cs
+++ Assets/playercontroller.cs
@@ -2,16 +2,5 @@ using UnityEngine;
 
 public class playercontroller : MonoBehaviour
 {
-    public Rigidbody rb;
-    public float speed = 12f;
-    void Update()
-    {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        Vector3 move = transform.right * x + transform.forward * z;
-
-        rb.linearVelocity = new Vector3(move.x, rb.linearVelocity.y, move.z) * speed * Time.deltaTime;
-
-    }
+    
 }
